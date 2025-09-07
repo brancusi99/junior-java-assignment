d8d62e53c09475e0488296d0c9b37a2def7ac46a
“Brinas <brinasdelia90@gmail.com>
2025-09-07 23:01:29 +0200

    D, added cron that logs expired policies

--- src/main/java/com/example/carins/service/InsurancePolicyService.java
+++ src/main/java/com/example/carins/service/InsurancePolicyService.java
@@ -2,11 +2,17 @@ package com.example.carins.service;
 
 import com.example.carins.model.InsurancePolicy;
 import com.example.carins.repo.InsurancePolicyRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class InsurancePolicyService {
     private final InsurancePolicyRepository insurancePolicyRepository;
+    private static final Logger logger = Logger.getLogger(InsurancePolicyService.class.getName());
 
     public InsurancePolicyService(InsurancePolicyRepository insurancePolicyRepository) {
         this.insurancePolicyRepository = insurancePolicyRepository;
@@ -14,6 +20,21 @@ public class InsurancePolicyService {
 
     public InsurancePolicy createOrUpdatePolicy(InsurancePolicy policy) {
         return insurancePolicyRepository.save(policy);
+    }
+
+    @Scheduled(cron = "0 */5 * * * *")
+    public void checkExpiredPolicies() {
+        LocalDate now = LocalDate.now();
+        List<InsurancePolicy> expiredPolicies = insurancePolicyRepository.findExpiredPolicies(now);
+
+        for (InsurancePolicy policy : expiredPolicies) {
+
+            if (!policy.isLogged()) {
+                logger.info(String.format("Policy %d for car %d expired on %s", policy.getId(), policy.getCar().getId(), policy.getEndDate()));
+                policy.setLogged(true);
+                insurancePolicyRepository.save(policy);
+            }
+        }
     }
 
 }
